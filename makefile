74231e8875300e9acbd3a37aa9a8d7b3d30654dc
Gareth Charnock <gareth.charnock@gmail.com>
2011-06-24 21:21:13 +0100

    Okay, that segfault looks like it was a bug in boost, which has been fixed in the latest version. For now, I've modifed the makefile to link to a local copy of boost_thread1.46

--- makefile
+++ makefile
@@ -1,14 +1,18 @@
 
 VTKLIBS = -lvtkalglib -lvtkCommon -lvtkDICOMParser -lvtkexoIIc -lvtkFiltering -lvtkftgl -lvtkGenericFiltering -lvtkGeovis -lvtkHybrid -lvtkImaging -lvtkInfovis -lvtkIO -lvtkmetaio -lvtkNetCDF -lvtkParallel -lvtkproj4 -lvtkRendering -lvtksqlite -lvtksys -lvtkverdict -lvtkViews -lvtkVolumeRendering -lvtkWidgets
 
-LIBS = -lgsl -lgslcblas -L../Cuba-2.1 -lcuba -lboost_thread
+LIBS = -lgsl -lgslcblas -L../Cuba-2.1 -lcuba -L. -lboost_thread-gcc44-mt-d-1_46_1 -lpthread
 
 OBJS = main.cpp data.cpp model.cpp #vis.cpp
+
+INCS = -I../Cuba-2.1 -I/usr/include/vtk-5.4/ -I../../src/boost_1_46_1
 
 #CFLAGS = -O3 -ffast-math
 CFLAGS = -g
 
 all:${OBJS}
-	g++ -Wall ${CFLAGS} ${OBJS} -o run  -I../Cuba-2.1 -I/usr/include/vtk-5.4/ ${VTKLIBS}  ${LIBS}  -Wno-deprecated
+	g++ -Wall ${CFLAGS} ${OBJS} -o run ${INCS}  ${VTKLIBS}  ${LIBS}  -Wno-deprecated
 	./run
 
+threads: threads.cpp
+	g++ -Wall threads.cpp -o threads ${LIBS}
--- threads.hpp
+++ threads.hpp
@@ -1,40 +1,26 @@
-
-
 #include <boost/thread.hpp>
 #include <boost/thread/barrier.hpp>
 #include <boost/function.hpp>
 #include <vector>
 
-template<typename T>
-class Multithreader;
-
-template<typename T>
-class WorkerThread {
-public:
-    WorkerThread(long _id,Multithreader<T>* _parent);
-    void operator()();
-    long id;
-    Multithreader<T>* parent;
-};
 
 
 template<typename T>
 class Multithreader {
-    friend class WorkerThread<T>;
 public:
     Multithreader()
 		: numCPU(sysconf(_SC_NPROCESSORS_ONLN)),
 		  _barrier(numCPU+1),
-		  mFuncs(NULL),
-          mMapTo(NULL){
+		  mFuncs(NULL) {
 		//Start the thread pool
 		for(long i = 0;i<numCPU;i++) {
-			boost::thread(new WorkerThread<T>(i,this));
+			boost::thread(WorkerThread(i,this));
 		}
 	}
 
 	~Multithreader() {
 		//Terminate the threads
+
 		//todo
 	}
 
@@ -42,8 +28,9 @@ public:
     void map(const std::vector<boost::function<T ()> >& funcs,std::vector<T>& mapTo) {
 		mFuncs = &funcs;
 		mMapTo = &mapTo;
+
 		_barrier.wait();
-        //Threads do work here
+        //Theads to work here
 		_barrier.wait();
     }
 private:
@@ -53,24 +40,23 @@ private:
 	const std::vector<boost::function<T ()> >* mFuncs;
 	std::vector<T>* mMapTo;
 
-
-
+	class WorkerThread {
+	public:
+		WorkerThread(long _id,Multithreader* _parent)
+			: id(id),parent(_parent) {
+		}
+		void operator()() {
+			while(true) {
+				parent->_barrier.wait();
+				//Do work
+				for(unsigned long i = 0;i<parent->mFuncs->size();i+=parent->numCPU) {
+					parent->mMapTo->at(i) = parent->mFuncs->at(i)();
+				}
+				parent->_barrier.wait();
+			}
+		}
+		long id;
+		Multithreader* parent;
+	};
 
 };
-
-template<typename T>
-WorkerThread<T>::WorkerThread(long _id,Multithreader<T>* _parent)
-    : id(id),parent(_parent) {
-}
-
-template<typename T>
-void WorkerThread<T>::operator()() {
-    while(true) {
-        parent->_barrier.wait();
-        //Do work
-        for(unsigned long i = 0;i<parent->mFuncs->size();i+=parent->numCPU) {
-            parent->mMapTo->at(i) = parent->mFuncs->at(i)();
-        }
-        parent->_barrier.wait();
-    }
-}
