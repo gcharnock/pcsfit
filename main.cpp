0dc44ba04cb8f4fa38939482beb11971d0f0a5d3
Gareth Charnock <gareth@Jaheira.(none)>
2011-07-15 18:21:08 +0100

    Object orientated minimiser template

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <boost/ref.hpp>
 #include <sstream>
 
+#include "foreach.hpp"
 #include "threads.hpp"
 #include "model.hpp"
 #include "data.hpp"
@@ -49,6 +50,80 @@ double cube_z_min, cube_z_max;
 
 ofstream fout;
 
+/********************************************************************************
+ * minimiser class
+ ********************************************************************************/
+
+template<typename T>
+class Minimiser {
+public:
+	typedef std::vector<double> PList;
+	Minimiser(boost::function<T(const PList&)> unpack,
+			  boost::function<PList(const T&)> pack,  
+			  boost::function<double(T)> fmin,		  
+			  boost::function<void(T)> logger)		  
+		: mUnpack(unpack),mPack(pack),mFMin(fmin),mLogger(logger) {
+	}
+
+	void minimise(T startingModel) {
+		//Unpack the starting model
+		PList plist = mPack(startingModel);
+		unsigned long nParams = plist.size();
+
+		//Initalise the minimiser
+		gsl_multimin_fminimizer* gslmin =
+			gsl_multimin_fminimizer_alloc(gsl_multimin_fminimizer_nmsimplex2,nParams);
+
+		gsl_multimin_function minfunc;
+		minfunc.f = &Minimiser::minf;
+		minfunc.n = nParams;
+		minfunc.params = (void*)this;
+
+		gsl_vector* vec = pList2GSLVec(plist);
+		gsl_vector* step_size = gsl_vector_alloc(plist.size());
+		
+		for(unsigned long i = 0;i<plist.size();i++) {
+			gsl_vector_set(step_size,i,plist[i] == 0 ? 1 : plist[i]*0.5);
+		}
+
+		gsl_multimin_fminimizer_set (gslmin, &minfunc, vec, step_size);
+		for(unsigned long i = 0; i<200;i++) {
+			gsl_multimin_fminimizer_iterate (gslmin);
+		}
+
+		gsl_multimin_fminimizer_free(gslmin);
+		gsl_vector_free(step_size);
+	}
+private:
+	double _minf(const gsl_vector * v) {
+		T params = unpack(gSLVec2pList(v));
+		mLogger(params);
+		return mFMin(params);
+	}
+	static double minf_static(const gsl_vector * v, void* _this) {
+		return ((Minimiser*)_this)->_minf(v);
+	}
+
+	static gsl_vector* pList2GSLVec(PList pList) {
+		gsl_vector* vec = gsl_vector_alloc (pList.size());
+		for(unsigned long i = 0; i<pList.size();i++) {
+			gsl_vector_set(vec,i,pList[i]);
+		}
+		return vec;
+	}
+	static PList gSLVec2pList(gsl_vector* vec) {
+		PList retVal;
+		for(size_t i = 0;i<vec->size;i++) {
+			retVal.push_back(gsl_vector_get(vec,i));
+		}
+		return retVal;
+	}
+
+	boost::function<T(const PList&)> mUnpack;
+	boost::function<PList(const T&)> mPack;
+	boost::function<double(T)> mFMin;
+	boost::function<void(T)> mLogger;	  
+};
 
 /********************************************************************************
  * Visuliser
